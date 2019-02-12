e8773d904db00513405e131753e06283c76c711c
LEE SEUNGWOO <jaeseung172@knou.ac.kr>
2019-02-13 03:08:17 +0900

    .

---
+++ DistanceConverter/DistanceConverter/FeetConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace DistanceConverter
+{
+    public static class FeetConverter
+    {
+        public static readonly double ratio = 0.3048;
+
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
+        }
+
+        public static double ToMeter(double feet)
+        {
+            return feet / ratio;
+        }
+    }
+}
---
+++ SalesCalculate/SalesCalculate/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculate
+{
+    class Sale
+    {
+        public string ShopName { get; set; }
+
+        public string ProductCategory { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
